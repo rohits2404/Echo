a830b4ffacc9e2fbd9488d02e7bc5f913532be25
Rohit <rohits8731@gmail.com>
2026-09-25 19:41:51 +0530

    07: Dashboard Layout

---
+++ apps/web/app/(dashboard)/customizations/page.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+const Page = () => {
+    return <div>Customizations</div>
+}
+
+export default Page
---
+++ apps/web/app/(dashboard)/plugins/vapi/page.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+const Page = () => {
+    return <p>Vapi Plugin</p>
+}
+
+export default Page
