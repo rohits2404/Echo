dde2378ea1b49fe221b2e0ad52a579af9653c1e4
Rohit <rohits8731@gmail.com>
2026-09-25 21:06:32 +0530

    09: Widget Layout

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,28 +1,16 @@
 "use client"
 
-import { Button } from "@workspace/ui/components/button"
-import { useVapi } from "@/modules/widget/hooks/use-vapi"
+import { WidgetView } from "@/modules/widget/ui/views/widget-view"
+import React, { use } from "react"
 
-export default function Page() {
-    const {
-        isSpeaking,
-        isConnecting,
-        isConnected,
-        transcript,
-        startCall,
-        endCall,
-    } = useVapi()
+interface Props {
+    searchParams: Promise<{
+        organizationId: string
+    }>
+}
 
-    return (
-        <div className="mx-auto flex min-h-svh w-full max-w-md flex-col items-center justify-center">
-            <Button onClick={() => startCall()}>Start Call</Button>
-            <Button onClick={() => endCall()} variant="destructive">
-                End Call
-            </Button>
-            <p>isConnected: {`${isConnected}`}</p>
-            <p>isConnecting: {`${isConnecting}`}</p>
-            <p>isSpeaking: {`${isSpeaking}`}</p>
-            <p>{JSON.stringify(transcript, null, 2)}</p>
-        </div>
-    )
+export default function Page({ searchParams }: Props) {
+    const { organizationId } = use(searchParams)
+
+    return <WidgetView organizationId={organizationId} />
 }
---
+++ apps/widget/modules/widget/ui/components/widget-footer.tsx
@@ -0,0 +1,39 @@
+import { Button } from "@workspace/ui/components/button"
+import { cn } from "@workspace/ui/lib/utils"
+import { HomeIcon, InboxIcon } from "lucide-react"
+import { useState } from "react"
+
+export const WidgetFooter = () => {
+    const [screen, setScreen] = useState<"selection" | "inbox">("selection")
+
+    return (
+        <footer className="flex items-center justify-between border-t bg-background">
+            <Button
+                className="h-14 flex-1 rounded-none"
+                onClick={() => setScreen("selection")}
+                size="icon"
+                variant="ghost"
+            >
+                <HomeIcon
+                    className={cn(
+                        "size-5",
+                        screen === "selection" && "text-primary"
+                    )}
+                />
+            </Button>
+            <Button
+                className="h-14 flex-1 rounded-none"
+                onClick={() => setScreen("inbox")}
+                size="icon"
+                variant="ghost"
+            >
+                <InboxIcon
+                    className={cn(
+                        "size-5",
+                        screen === "inbox" && "text-primary"
+                    )}
+                />
+            </Button>
+        </footer>
+    )
+}
