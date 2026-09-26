b80a49e10a7023968b006f18a736e95c0ba71472
Rohit <rohits8731@gmail.com>
2026-09-27 01:32:44 +0530

    28: Widget Inprovements

--- apps/widget/modules/widget/ui/views/widget-view.tsx
+++ apps/widget/modules/widget/ui/views/widget-view.tsx
@@ -9,6 +9,7 @@ import { WidgetSelectionScreen } from "../screens/widget-selection-screen"
 import { WidgetChatScreen } from "../screens/widget-chat-screen"
 import { WidgetInboxScreen } from "../screens/widget-inbox-screen"
 import { WidgetVoiceScreen } from "../screens/widget-voice-screen"
+import { WidgetContactScreen } from "../screens/widget-contact-screen"
 
 interface Props {
     organizationId: string | null
@@ -25,12 +26,11 @@ export const WidgetView = ({ organizationId }: Props) => {
         inbox: <WidgetInboxScreen />,
         selection: <WidgetSelectionScreen />,
         chat: <WidgetChatScreen />,
-        contact: <p>TODO: Contact</p>,
+        contact: <WidgetContactScreen />,
     }
 
     return (
-        // TODO: Confirm whether or not "min-h-screen" and "min-w-screen" is needed
-        <main className="flex h-full min-h-screen w-full min-w-screen flex-col overflow-hidden rounded-xl border bg-muted">
+        <main className="flex h-full w-full flex-col overflow-hidden rounded-xl border bg-muted">
             {screenComponents[screen]}
         </main>
     )
